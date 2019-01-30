84a0840b0521845f61643ea221a9bd2d42c5095f
LaisFelipe <47155099+LaisFelipe@users.noreply.github.com>
2019-01-29 22:04:14 -0200

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,6 @@
+public class MathUtils { 
+	public static double Average(int a, int b) { 
+		return (a + b) / 2.0; } 
+	public static void Main(string[] args) { 
+		Console.WriteLine(Average(2, 1)); } }
+
